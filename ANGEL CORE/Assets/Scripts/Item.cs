1193b9fa227deab035eef774106594456cf8bc27
Avi Nautiyal <avomanatee@gmail.com>
2025-01-06 01:04:41 -0500

    Basic health system !
    Working on a jump boost little ball

---
+++ ANGEL CORE/Assets/Scripts/Item.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    
+}
