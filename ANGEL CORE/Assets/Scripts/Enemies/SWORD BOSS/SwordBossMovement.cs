aadd0adefda16acf1b2c5248b71fa8dec720bf15
Avi Nautiyal <avomanatee@gmail.com>
2025-01-24 17:01:40 -0500

    Sword boss in production

--- ANGEL CORE/Assets/Scripts/Enemies/SWORD BOSS/SwordBossMovement.cs
+++ ANGEL CORE/Assets/Scripts/Enemies/SWORD BOSS/SwordBossMovement.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class SwordBossMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    HealthManager healthman;
+    Vector3 targetLocation;
+    float timer = 5f;
+    public float speed;
+    Transform target;
+    public int phase;
+    float hp;
+    public bool _swingLock;
+
+
+
     void Start()
     {
+        phase = 1;
+        healthman = GetComponent<HealthManager>();
+        hp = healthman.curHealth;
         
+
+        target = GameObject.Find("Phase 1 Target").transform;
+
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(_swingLock)
+        {
+            SwingLock();
+        }
+
+    }
+
+    void defaultMvmt()
+    {
+
+    }
+    void SwingLock()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed);
+
+    }
+
+    void spawnBabies()
+    {
+
     }
 }
