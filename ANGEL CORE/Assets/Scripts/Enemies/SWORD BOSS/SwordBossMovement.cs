111c644a3ed68b66dec369a99a77336a591ca241
Avi Nautiyal <avomanatee@gmail.com>
2025-01-24 20:59:24 -0500

    bruhh boss movement

--- ANGEL CORE/Assets/Scripts/Enemies/SWORD BOSS/SwordBossMovement.cs
+++ ANGEL CORE/Assets/Scripts/Enemies/SWORD BOSS/SwordBossMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SwordBossMovement : MonoBehaviour
 {
+    GameObject pointHolder;
+    List<Vector3> points = new List<Vector3>();
     HealthManager healthman;
     Vector3 targetLocation;
     float timer = 5f;
@@ -13,6 +15,7 @@ public class SwordBossMovement : MonoBehaviour
     float hp;
     public bool _swingLock;
     bool slashing;
+    bool hasTarget;
 
 
 
@@ -22,7 +25,14 @@ public class SwordBossMovement : MonoBehaviour
         phase = 1;
         healthman = GetComponent<HealthManager>();
         hp = healthman.curHealth;
-        
+        pointHolder = GameObject.Find("PowerPoints");
+        for(int i = 0; i < pointHolder.transform.childCount; i++)
+        {
+            points.Add(pointHolder.transform.GetChild(i).position);
+        }
+
+
+        target = GameObject.Find("Player").transform;
 
         target = GameObject.Find("Phase 1 Target").transform;
 
@@ -36,7 +46,11 @@ public class SwordBossMovement : MonoBehaviour
     {
         timer -= Time.deltaTime;
         
-
+        if(!_swingLock)
+        {
+            defaultMvmt();
+            GetTarget();
+        }
 
         if(_swingLock)
         {
@@ -47,7 +61,24 @@ public class SwordBossMovement : MonoBehaviour
 
     void defaultMvmt()
     {
+        if (hasTarget)
+        {
+            transform.position = transform.position + (Vector3.Normalize(targetLocation - transform.position) * speed * Time.deltaTime);
+        }
+    }
+    void GetTarget()
+    {
+        if (!hasTarget)
+        {
+            targetLocation = points[Random.Range(0, points.Count)];
+            hasTarget = true;
+        }
+        if (Vector3.Distance(targetLocation, transform.position) < 1f)
+        {
+            timer -= Time.deltaTime;
 
+            if(timer <= 0) {hasTarget = false; timer = 5f;}
+        }       
     }
     void SwingLock()
     {
